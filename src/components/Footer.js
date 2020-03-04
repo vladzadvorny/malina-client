c06396da8f6c98629d6a2832775c30ed927635e2
Vlad Zadvorny <vladzadvorny@outlook.com>
2020-03-04 12:33:24 +0300

    footer menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,57 @@
 import { h } from 'preact'
+import { Link } from 'preact-router'
 
 import './Footer.scss'
+import { Fire, Search, Comments } from '../utils/icons'
+
+const MenuItem = ({ href, Icon, isActive }) => {
+  const props = isActive ? { className: 'active' } : {}
+
+  return (
+    <li>
+      <Link href={href} {...props}>
+        <Icon />
+      </Link>
+    </li>
+  )
+}
 
 const Footer = () => {
+  const pathname =
+    typeof window !== 'undefined' ? window.location.pathname.split('/')[1] : ''
+
+  const menu = [
+    {
+      href: '/',
+      icon: Fire,
+      isActive: pathname === ''
+    },
+    {
+      href: '/find',
+      icon: Search,
+      isActive: pathname === 'find'
+    },
+    {
+      href: '/chat-list',
+      icon: Comments,
+      isActive: pathname === 'chat-list'
+    }
+  ]
+
   return (
     <footer className="footer">
-      <div className="container">footer</div>
+      <div className="container">
+        <ul className="menu">
+          {menu.map(item => (
+            <MenuItem
+              key={item.href}
+              href={item.href}
+              Icon={item.icon}
+              isActive={item.isActive}
+            />
+          ))}
+        </ul>
+      </div>
     </footer>
   )
 }
